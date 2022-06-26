89ed26597319e4b84527e314e494eb3d0b171009
Robinerra <greggoryg@protonmail.com>
2022-06-26 16:29:21 -0700

    Patch security issue

--- components/githubProject.tsx
+++ components/githubProject.tsx
@@ -15,13 +15,17 @@ function githubProject(props: any) {
   let icons1: any = [];
 
   props.icons.forEach((i: string) => {
-    icons1.push(<i className={`${i} ${style.projectIcon}` } />);
+    icons1.push(<i className={`${i} ${style.projectIcon}`} />);
   });
 
   return (
     <div className={style.projectContainer}>
       <h3>
-        <i className="devicon-github-original"/> <a target={"_blank"} href={props.link}>{props.name}</a> {icons1}
+        <i className="devicon-github-original" />{" "}
+        <a target={"_blank"} rel="noreferrer" href={props.link}>
+          {props.name}
+        </a>{" "}
+        {icons1}
       </h3>
       <p>{props.description}</p>
     </div>
